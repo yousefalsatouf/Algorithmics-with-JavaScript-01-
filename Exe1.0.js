ccbe736d34d1b046fc4e585624d87c3bbfc5cc66
[Yousef Alsatouf] <yousef.alsatouf94@gmail.com>
2019-08-20 14:43:45 +0200

    this is the exes of js, and this is the first commit

---
+++ Exe1.0.js
@@ -0,0 +1,6 @@
+/*
+    Exercise 1.0 - Interactive Mode
+
+Launch node in interactive mode and use it to make an addition. Then repeat the same step until you have tested all five arithmetic operators we presented before.
+Wonderful ! From now on you'll never need a calculator anymore.
+*/
